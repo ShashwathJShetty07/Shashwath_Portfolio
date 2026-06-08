2838fa56b520c58c0f26a47a2900d0735516486d
ShashwathJShetty07 <shashwathjshetty@gmail.com>
2026-06-09 04:50:27 +0530

    Fix Vite entry

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
         portfolio: resolve(__dirname, 'portfolio.html'),
       },
     },
